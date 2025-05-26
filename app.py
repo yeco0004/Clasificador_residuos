a12221500c128a2ffa80569e4d9e3f9dd45818d1
yeco0004 <63363566+yeco0004@users.noreply.github.com>
2025-05-25 19:53:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import streamlit as st
 from PIL import Image
 
-st.title("Prueba básica")
+st.title("Prueba básica de subida y muestra de imagen")
 
-uploaded_file = st.file_uploader("Sube imagen", type=["png", "jpg"])
+uploaded_file = st.file_uploader("Sube una imagen", type=["png", "jpg", "jpeg"])
 
 placeholder = st.empty()
 
 if uploaded_file:
     img = Image.open(uploaded_file)
-    placeholder.image(img)
+    placeholder.image(img, caption="Imagen subida", use_column_width=True)
 else:
     placeholder.empty()
 
